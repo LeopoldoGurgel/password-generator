755e623b211649c3b897b76234f24381b0876bb5
LeopoldoGurgel <120462695+LeopoldoGurgel@users.noreply.github.com>
2023-08-11 06:54:44 -0400

    password generator v1.0

---
+++ Develop/script.js
@@ -0,0 +1,104 @@
+// Assignment code here
+
+function generatePassword() {
+
+// STRINGS CONTAINING DIFFERENT TYPES 
+// OF CHARACTERS. THEY WILL BE ACCESSED 
+// LATER BY THE FUNCTIONS THAT WILL ASSEMBLE 
+/// THE PASSWORD.
+
+  var numbers = "0123456789";
+  var lcLetters = "abcdefghijklmnopqrstuvwxyz";
+  var ucLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specials = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  var chosenChars = "";
+
+  var newPassword = "";
+
+
+  // QUESTIONS FOR THE USER TO CHOOSE THE LENGTH OF THE 
+  // PASSWORD AND WHAT CHARACTERS THEY WANT TO ADD TO IT.
+  
+  // PROMPTS ALWAYS RETURN A STRING. I NEEDED A NUMBER IN ORDER 
+  // TO USE IT IN A FOR LOOP LATER IN THE CODE. THAT GAVE
+  // ME A BIT OF A HARD TIME. STACKOVERFLOW PRESENTED ME WITH
+  // THE FOLLOWING OPTIONS THAT COULD DO THAT: parseInt(), 
+  // number(), parseFloat() and unary. SINCE UNARY IS JUST
+  // ADDING A "+" BEFORE THE PROMPT, IT SEEMED TOO GOOD TO BE
+  // TRUE. SO I DIDN'T TRUST IT AND WENT FOR PARSEINT.
+
+  var passwordLength = parseInt(prompt("How long do you want your password to be? Choose a value between 8 and 128", " "));
+  
+  var lengthErr;
+
+  if(passwordLength < 8 || passwordLength > 120) {
+    lengthErr = confirm("ERROR! Choose a password length between 8 and 120 characters!");
+    generatePassword();
+    return;
+  }
+
+// ADDING THE DESIRED CHARACTERS TO THE 
+// VARIABLE chosenChars. THAT WILL BE USED
+// TO BUILD THE PASSWORD.
+
+var addLowercase = confirm("Do you want lowercase characters in your password? Click on 'OK' for YES or on 'CANCEL' for NO.");
+var addUppercase = confirm("Do you want uppercase characters in your password? Click on 'OK' for YES or on 'CANCEL' for NO.");
+var addNumbers = confirm("Do you want to add numbers to your password? Click on 'OK' for YES or on 'CANCEL' for NO.");
+var addSpecials = confirm("Do you want to add special characters to your password? Click on 'OK' for YES or on 'CANCEL' for NO.");
+
+var noCharErr;
+
+if(addLowercase) {
+  chosenChars = chosenChars + lcLetters;
+};
+
+if (addUppercase) {
+  chosenChars = chosenChars + ucLetters;
+};
+
+if (addNumbers) {
+  chosenChars = chosenChars + numbers;
+};
+
+if (addSpecials) {
+  chosenChars = chosenChars + specials;
+};
+
+if (!addLowercase && !addNumbers && !addSpecials && !addUppercase) {
+  noCharErr = confirm("ERROR! You didn't select any type of characters. You have to select at least one type!");
+  generatePassword();
+  return;
+}
+
+// i was making these all into a if/else if statement. But somehow
+// it was not working as expected and i had to make it into
+// different ifs. Gotta work through that with Sal later.
+
+
+// NOW WE STEP INTO ACTUALLY BUILDING OUR PASSWORD
+
+for( var i = 0; i < passwordLength; i++) {
+  var randomChar = Math.floor(Math.random() * chosenChars.length);
+  newPassword = newPassword + chosenChars.charAt(randomChar);
+};
+
+return newPassword;
+
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,39 @@
+# Password Generator Starter Code
+
+## DESCRIPTION
+
+This project delivers a password generator using some brain frying Javascript logic that took a lot of time to figure out, to research and to write it down. At least now you can put aside that "av0cad0" password. Make good use of this tool.
+
+![Screenshot of the app](./Develop/images/PWGscreenshot.jpg "Page screenshot")
+
+## RESOURCES
+
+A lot of cool new features were added to the script of this project. Most of them came from UofT coding bootcamp, such as alert, prompt and confirm methods, if statments, building strings, math.floor and math.random. But at some point I had to make a string into a number and that came from StackOverflow.
+
+## INSTRUCTIONS
+
+At the landing page, you will find a very intuitive user interface for the web app. It has the title at the top, a box in which your password will appear and a big red button with "Generate Password" written on it. When you click that button, some questions will appear on your screen. Answer the questions following the instructions in each of them and you will have a super safe password in the end of the process.
+
+## LICENSE
+
+MIT License
+
+Copyright (c) [2023] [Leopoldo Gurgel Barroso Pimentel]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
